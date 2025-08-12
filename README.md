b2ad97e1e202e34021b29a3a3eeae439716bde81
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-12 13:07:53 +0200

    chore(init): initialize plugin

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+<h1>WordPress Pretix Integration</h1>
+<h3>pretix-integration</h3>
+<p>🧩 A plugin for integrating Pretix into Wordpress</p>
+<a href="LICENSE">
+<img alt="License" src="https://img.shields.io/github/license/unikino-gegenlicht/wordpress-pretix-sync?style=for-the-badge">
+</a>
+<img alt="GitHub Issues or Pull Requests" src="https://img.shields.io/github/issues/unikino-gegenlicht/wordpress-pretix-sync?style=for-the-badge">
+</div>
+
+This plugin allows linking movies and events to pretix pages.
+It amends the custom post types we use and allows manually specifying the url on which tickets for this event are
+available on.
+
+## Roadmap
+
+- [ ] Automatic Creation of Events in Pretix
+- [ ] Automatic Update of Event in Pretix (incl. notifying ticket holders of changes)
---
+++ wordpress-pretix-sync.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+Plugin Name: Wordpress Pretix Sync
+Plugin URI: https://github.com/unikino-gegenlicht/wordpress-pretix-sync
+Description: Allow linking to pretix for ticket sales
+Version: 1.0
+Author: Jan Eike Suchard
+Author URI: https://suchard.cloud
+License: GPL3
+*/
+
+function extend_metaboxes( $meta_boxes ): mixed {
+	$meta_boxes[] = [
+		'title'      => esc_html__( 'Reservations', 'wordpress-pretix-sync' ),
+		'id'         => 'reservations',
+		'context'    => 'side',
+		'post_types' => [ 'movie', 'event' ],
+		'autosave'   => true,
+		'fields'     => [
+			[
+				'type'      => 'switch',
+				'name'      => esc_html__( 'Allow Reservations', 'wordpress-pretix-sync' ),
+				'id'        => 'allow_reservations',
+				'on_label'  => esc_html__( 'Yes', 'wordpress-pretix-sync' ),
+				'off_label' => esc_html__( 'No', 'wordpress-pretix-sync' ),
+			],
+			[
+				'type' => 'url',
+				'name' => esc_html__( 'URL to Pretix Page' ),
+				'id'   => "reservation_url",
+				'visible' => ["allow_reservations"],
+			]
+		]
+
+	];
+
+	return $meta_boxes;
+}
+
+add_filter( 'rwmb_meta_boxes', 'extend_metaboxes' );
